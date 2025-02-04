7e5923970fd23570be64fd7bb82d2ca5886b8a30
BlackOwlHU <rolnadmester@gmail.com>
2025-02-04 09:36:56 +0100

    a0.4
    Added password changing part, but without the main javascript part.

--- js/profile.js
+++ js/profile.js
@@ -1,23 +1,10 @@
 const iconLogout = document.getElementsByClassName('icon-logout')[0];
 const iconHome = document.getElementsByClassName('icon-home')[0];
-const btnPic = document.getElementsByClassName('edit-pic')[0];
-const btnName = document.getElementById('editName');
+const btnData = document.getElementById('editPost');
 const btnPsw = document.getElementById('editPsw');
+const btnOrders = document.getElementById('orders');
 
 iconLogout.addEventListener('click', logout);
-
-// profilkép megjelenítése
-async function getProfilePic() {
-    const res = await fetch('http://127.0.0.1:3000/api/profile/getProfilePic', {
-        method: 'GET',
-        credentials: 'include'
-    });
-    const data = await res.json();
-    //console.log(data);
-    if(res.ok){
-        btnPic.style.backgroundImage = `url(http://127.0.0.1:3000/uploads/${data[0].profile_pic})`;
-    };
-};
 
 async function logout() {
     const res = await fetch('http://127.0.0.1:4000/api/auth/logout', {
@@ -38,10 +25,14 @@ iconHome.addEventListener('click', () => {
     window.location.href = '../homepage/home.html';
 });
 
-btnName.addEventListener('click', () => {
-    window.location.href = '../profileName.html';
+btnData.addEventListener('click', () => {
+    window.location.href = '../profile/profileInfo.html';
 });
 
 btnPsw.addEventListener('click', () => {
-    window.location.href = '../profilePsw.html';
+    window.location.href = '../profile/profilePsw.html';
+});
+
+btnOrders.addEventListener('click', () => {
+    window.location.href = '../homepage/home.html';
 });
