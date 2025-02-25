e671c51ea5125bcabe00cbcf51c9ae42ab56284b
BlackOwlHU <rolnadmester@gmail.com>
2025-02-25 11:28:42 +0100

    0.41

---
+++ js/adminOrders.js
@@ -0,0 +1,23 @@
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm';
+
+const iconLogout = document.getElementsByClassName('icon-logout')[0];
+
+iconLogout.addEventListener('click', logout);
+
+async function logout() {
+    const res = await fetch('http://127.0.0.1:4000/api/auth/logout', {
+        method: 'POST',
+        credentials: 'include'
+    });
+
+    if (res.ok) {
+        window.location.href = "../relog/index.html";
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Hiba történt a kijelentkezés során",
+            draggable: false
+        });
+    }
+};
