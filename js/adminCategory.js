8a9ffd3250b897f354ee9de079c98fe312db0535
BlackOwlHU <rolnadmester@gmail.com>
2025-03-05 11:18:37 +0100

    working on adminCategory again again back again 2

--- js/adminCategory.js
+++ js/adminCategory.js
@@ -79,7 +79,7 @@ async function renderCategories(categoryList) {
         showCategories.innerHTML += `
             <div class="order card">
                 <strong>Kategória: ${category.category}</strong>
-                <img src="/uploads/https://nodejs.dszcbaross.edu.hu/server/95f964bc/files#/RolIpon_BackEnd/uploads${category.image}" alt="${category.category}">
+                <img src="https://nodejs.dszcbaross.edu.hu/server/95f964bc/files#/RolIpon_BackEnd/uploads${category.image}" alt="${category.category}">
                 <i class="fa-solid fa-trash trash data" data-category-id="${category.id}"></i>
             </div>`;
     }
